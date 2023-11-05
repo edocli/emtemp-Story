711eb672734c7cbeeccc9ff43135406241366458
UTF-X <utf-x@outlook.com>
2023-11-05 12:56:42 -0500

    [feat] 添加对页面进行评论的支持

--- page.php
+++ page.php
@@ -13,6 +13,17 @@ if (!defined('EMLOG_ROOT')) {
                     <div class="post-content" itemprop="articleBody">
                         <?= $log_content ?>
                     </div>
+
+                    <div id="postFun" style="display:block;margin-bottom:2em;" class="clearfix">
+                        <section style="float:right;">
+                            <span><a id="btn-comments" href="javascript:isComments();">show comments</a></span> ·
+                            <span><a href="javascript:goBack();">back</a></span> ·
+                            <span><a href="<?= BLOG_URL ?>">home</a></span>
+                        </section>
+                    </div>
+
+                    <?php require_once View::getView('comments'); ?>
+
                 </article>
             </div>
         </div>
